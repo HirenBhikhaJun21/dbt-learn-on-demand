ca8460e33c97dcd88f805b3b0c299f4695f0664a
hiren.bhikha.Jun21@outlook.com <hiren.bhikha.Jun21@outlook.com>
2021-07-16 04:30:12 +0000

    with_Test

--- models/staging/jaffle_shop/stg_order_pmt.sql
+++ models/staging/jaffle_shop/stg_order_pmt.sql
@@ -1,7 +1,7 @@
 SELECT b.ORDER_ID
        ,b.customer_id
-       ,a.amount/100 as amount
-FROM "RAW"."STRIPE"."PAYMENT" as a
-inner JOIN "TEST"."DBT_HBHIKHA"."STG_ORDERS" as b
+       ,a.amount/100 as amount   
+FROM {{ source('stripe', 'payment') }} as a
+inner JOIN  {{ ref('stg_orders') }}  as b
     on a.ORDERID = b.ORDER_ID
 WHERE a.status = 'success'
